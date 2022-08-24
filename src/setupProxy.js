80d150fcf22e0d9d03252fbbe9858979eb583fb0
Akame <kaixuang@student.unimelb.edu.au>
2022-08-24 20:32:27 +0800

    readme

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -3,7 +3,7 @@ const {createProxyMiddleware} = require('http-proxy-middleware');
 
 module.exports = function(app) {
     app.use('/api', createProxyMiddleware({
-        target: 'https://localhost:8081',//后台服务器地址
+        target: 'https://192.168.3.43:8081',//后台服务器地址
         secure: false,
         changeOrigin: true,
         pathRewrite: {
